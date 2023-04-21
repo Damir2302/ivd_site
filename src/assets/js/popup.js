eaed5e8828a55f11b9b49bca1c1b8101d28495b5
Damir2302 <damir23.02@mail.ru>
2023-04-21 17:30:56 +0300

    about page

--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -2,13 +2,26 @@ $(document).ready(function () {
 
     // ДЛЯ ВСЕХ(!!!!!!) ВСПЛЫВАЮЩИХ ОКОН [Если кликаем за пределы всплывающего окна, закрываем это окно]
     $("html").on("click", function (e) {
-        if (
-            !$(e.target).closest(".navbar").length
-        ) {
+        if (!$(e.target).closest('.nav').length &&
+            !$(e.target).closest('.header__burger').length) {
+
             $("body").removeClass("overflow-hidden")
             $('#page').removeClass('bg-overlay')
+
+            $('.navbar').removeClass('opened')
+            $('.header__burger').removeClass('opened')
         }
     });
 
+    function openMobMenu() {
+        $('.header__burger').on('click', function() {
+            $('body').toggleClass('overflow-hidden')
+            $('#page').toggleClass('bg-overlay')
+            $(this).toggleClass('opened')
+            $('.navbar').toggleClass('opened')
+        })
+    }
+
+    openMobMenu();
 
 });
--- src/assets/js/sliders.js
+++ src/assets/js/sliders.js
@@ -1,4 +1,81 @@
 $(document).ready(function() {
 
+    // HERO SLIDER
+    let heroSlider;
+
+    heroSliderRun();
+
+    function heroSliderRun() {
+        heroSlider = new Swiper(".hero__slider .swiper", {
+            slidesPerView: 1,
+
+            navigation: {
+                nextEl: '.hero__arrow--next',
+                prevEl: '.hero__arrow--prev',
+            },
+
+            pagination: {
+                el: ".slider-pagination",
+                clickable: true,
+            }
+        })
+    }
+
+    // ABOUT PAGE
+    let aboutClientsSlider = new Swiper('.about__clients .swiper', {
+        slidesPerView: 2,
+        spaceBetween: 10,
+        loop: true,
+
+        breakpoints: {
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+
+            744: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
+
+            1400: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            }
+        },
+
+        navigation: {
+            nextEl: '.about__clients .swiper-nav-next',
+            prevEl: '.about__clients .swiper-nav-prev',
+        }
+    })
+
+    let aboutReviewsSlider = new Swiper('.about__reviews .swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 10,
+        loop: true,
+
+        breakpoints: {
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+
+            744: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
+
+            1400: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            }
+        },
+
+        navigation: {
+            nextEl: '.about__reviews .swiper-nav-next',
+            prevEl: '.about__reviews .swiper-nav-prev',
+        }
+    })
 
 })
